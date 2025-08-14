5ffa03ef6b3cdd9f002c7c4a7efbe034261ee573
AlexKCoherent <alexkaraychetsev@coherentsolutions.com>
2025-08-14 10:54:23 +0300

    Update import logic

--- src/lotsInfo.js
+++ src/lotsInfo.js
@@ -1,59 +1,63 @@
-import Paint1 from "../src/img/lots/1.jpeg";
-import Paint2 from "../src/img/lots/2.jpeg";
-import Paint3 from "../src/img/lots/3.jpeg";
-import Paint4 from "../src/img/lots/4.jpeg";
-import Paint5 from "../src/img/lots/5.jpeg";
-import Paint6 from "../src/img/lots/6.jpeg";
-import Paint7 from "../src/img/lots/7.jpeg";
-import Paint8 from "../src/img/lots/8.jpeg";
+// import Paint1 from "../src/img/lots/1.jpeg";
+// import Paint2 from "../src/img/lots/2.jpeg";
+// import Paint3 from "../src/img/lots/3.jpeg";
+// import Paint4 from "../src/img/lots/4.jpeg";
+// import Paint5 from "../src/img/lots/5.jpeg";
+// import Paint6 from "../src/img/lots/6.jpeg";
+// import Paint7 from "../src/img/lots/7.jpeg";
+// import Paint8 from "../src/img/lots/8.jpeg";
+
+const paints = Array.from({ length: 8 }, (_, i) =>
+  require(`../src/img/lots/${i + 1}.jpeg`)
+);
 
 export const LotsInfo = [
   {
     id: 1,
     name: "Банка котурцов",
-    img: Paint1,
+    img: paints[0],
     size: "30х40см",
   },
   {
     id: 2,
     name: "Париж",
-    img: Paint3,
+    img: paints[2],
     size: "15х20см",
   },
   {
     id: 3,
     name: "Утро",
-    img: Paint5,
+    img: paints[4],
     size: "30х40см",
   },
   {
     id: 4,
     name: "Бесконечность",
-    img: Paint6,
+    img: paints[5],
     size: "30х40см",
   },
   {
     id: 5,
     name: "Котовик обыкновенный",
-    img: Paint7,
+    img: paints[6],
     size: "30х40см",
   },
   {
     id: 6,
     name: "Бджола",
-    img: Paint4,
+    img: paints[3],
     size: "30х30см",
   },
   {
     id: 7,
     name: "Рабочее утро",
-    img: Paint2,
+    img: paints[1],
     size: "30х30см",
   },
   {
     id: 8,
     name: "Нян кэт",
-    img: Paint8,
+    img: paints[7],
     size: "30х30см",
   },
 ];
