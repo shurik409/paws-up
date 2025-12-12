f4e1485eec29714887b0585e0ee6aea9734d2e26
AlexKCoherent <alexkaraychetsev@coherentsolutions.com>
2025-12-12 17:14:16 +0300

    fix server

--- server.js
+++ server.js
@@ -126,11 +126,8 @@ app.get("/count/", function (request, response) {
 });
 
 app.get("/auction/lot/:id", function (request, response) {
-  const id = Number(request.params.id);
-  if (id === 1) {
-    return response.sendFile(path.join(__dirname, "build", "index.html"));
-  }
-  response.sendStatus(404);
+  // const id = Number(request.params.id);
+  return response.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.get("/auction/results/:id", function (request, response) {
