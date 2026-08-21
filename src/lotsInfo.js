b48f9174fe3beab6f8a1b3590082057ac46314eb
AlexKCoherent <alexkaraychetsev@coherentsolutions.com>
2026-08-21 20:32:56 +0300

    Update paintings

--- src/lotsInfo.js
+++ src/lotsInfo.js
@@ -1,55 +1,55 @@
-const paints = Array.from({ length: 7 }, (_, i) =>
+const paints = Array.from({ length: 6 }, (_, i) =>
   require(`../src/img/lots/${i + 1}.jpeg`),
 );
 
 export const LotsInfo = [
   {
     id: 1,
-    name: "@&”%\#",
+    name: "Joey doesn't share food",
     img: paints[0],
     size: "30х40см",
     isVertic: true,
   },
   {
     id: 2,
-    name: "But Why?",
+    name: "Собачий кайф",
     img: paints[1],
     size: "30х40см",
     isVertic: true,
   },
   {
     id: 3,
-    name: "А тебя это * не должно",
+    name: "Кот в мешке",
     img: paints[2],
     size: "30х40см",
     isVertic: true,
   },
   {
     id: 4,
-    name: "Я худею",
-    img: paints[3],
+    name: "Ну мы",
+    img: paints[5],
     size: "30х40см",
     isVertic: true,
   },
   {
     id: 5,
-    name: "Я с моими пацанами",
+    name: "Лесная вечеринка",
     img: paints[4],
-    size: "40х30см",
+    size: "30х40см",
     isVertic: true,
   },
   {
     id: 6,
-    name: "Фея",
-    img: paints[5],
+    name: "Бибиди бобиди бум",
+    img: paints[3],
     size: "30х30см",
     isVertic: true,
   },
-  {
-    id: 7,
-    name: "Я звезда youtube",
-    img: paints[6],
-    size: "30х40см",
-    isVertic: true,
-  },
+  // {
+  //   id: 7,
+  //   name: "Я звезда youtube",
+  //   img: paints[6],
+  //   size: "30х40см",
+  //   isVertic: true,
+  // },
 ];
